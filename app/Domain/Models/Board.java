30955a58fb2fe40041c7ea7f9fc8ea07d938dbaf
Itay Maoz <itaym2@gmail.com>
2014-02-15 00:46:13 +0200

    Add mongojack-play and start working on domain

---
+++ app/Domain/Models/Board.java
@@ -0,0 +1,31 @@
+package Domain.Models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.mongojack.ObjectId;
+
+public class Board  {
+
+    @JsonProperty("name")
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ObjectId
+    @JsonProperty("_id")
+    public String getId() {
+        return id;
+    }
+
+    @ObjectId
+    @JsonProperty("_id")
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ app/controllers/Boards.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by itay on 2/15/14.
+ */
+public class Boards extends Controller {
+    public Result get(String id) {
+
+        return ok();
+    }
+}
