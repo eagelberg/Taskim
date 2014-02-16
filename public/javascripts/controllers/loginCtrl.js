90ddc1eb0f6ad94ee57ae54619106c3cb6bfe768
Itay Maoz <itaym2@gmail.com>
2014-02-16 12:54:52 +0200

    start designing the board page

--- public/javascripts/controllers/loginCtrl.js
+++ public/javascripts/controllers/loginCtrl.js
@@ -1,3 +1,9 @@
-taskimApp.controller('loginCtrl',['$scope',function($scope){
+taskimApp.controller('loginCtrl',['$scope', '$http', function($scope, $http){
+      $scope.getBoard = function () {
+          $http.get('/Boards/52ff5e6044ae2e25ae9712ae').success(function (data) {
+              $scope.board = data;
+          })
+      }
 
+    $scope.board = []
 }])
