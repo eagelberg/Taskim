2e70d41c33bf373d3c12b86cf72b8668932849c1
guy eagelberg <eagelberg@gmail.com>
2014-02-17 20:10:06 +0200

    add angular ui and login page

--- public/app.js
+++ public/app.js
@@ -1,15 +1,38 @@
 var taskimApp = angular.module("taskimApp", ["mgcrea.ngStrap.navbar",'ui.router'])
-    .config(["$routeProvider", function($routeProvider) {
-         $routeProvider.state("login", {
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/login");
+
+        $stateProvider.state("login", {
             url: '/login',
             templateUrl: 'assets/partials/loginPage.html',
             controller: 'loginCtrl'
-        }).otherwise({
-                redirectTo: "/login"
-            });
+        })
+            .state("menu",{
+            url: '/menu',
+                templateUrl: 'assets/partials/loginPage.html',
+                controller: 'loginCtrl'
+        });
     }
     ])
     .config(["$locationProvider", function($locationProvider) {
          $locationProvider.html5Mode(true).hashPrefix("!");
+    }]);
+
+taskimApp.service('loggedUserService',function(){
+    this.loggedUser = null;
+
+    this.login = function(name,password){
+        //todo validate user
+        this.loggedUser = {name : 'fsd' ,permissions : 'public'};
     }
-    ]);
+});
+
+taskimApp.run(['$rootScope',"$location",'loggedUserService', function($rootScope,$location,loggedUserService) {
+        $rootScope.$on( "$stateChangeStart", function(event, next, current) {
+            if ( loggedUserService.loggedUser == null) {
+                if ( next.templateUrl != "assets/partials/loginPage.html" ) {
+                    $location.path( "/login" );
+                }
+            }
+        });
+    }]);
--- public/javascripts/controllers/loginCtrl.js
+++ public/javascripts/controllers/loginCtrl.js
@@ -1,3 +1,18 @@
-taskimApp.controller('loginCtrl',['$scope',function($scope){
+taskimApp.controller('loginCtrl',['$scope','loggedUserService',function($scope,loggedUserService){
+    $scope.user = {
+        name : '',
+        password : ''
+    };
 
+
+    $scope.login = function(){
+        loggedUserService.login($scope.name,$scope.password);
+    }
+
+    $scope.reset = function(){
+        $scope.user = {
+            name : '',
+            password : ''
+        };
+    }
 }])
