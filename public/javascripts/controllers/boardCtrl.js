d8adf8351e6ca7c9202d05f34d9888b978c90877
Itay Maoz <itaym2@gmail.com>
2014-02-21 13:50:31 +0200

    make the board page scrollable

--- public/javascripts/controllers/boardCtrl.js
+++ public/javascripts/controllers/boardCtrl.js
@@ -1,10 +1,23 @@
 taskimApp.controller('boardCtrl',['$scope','boardManager',function($scope,boardManager){
 
     $scope.board = {};
+    $scope.canvasWidth = 100;
+
+    $scope.getCanvasStyle = function() {
+        return {width: $scope.canvasWidth + 'px'};
+    }
+
+    $scope.$watch('canvasWidth', function(){
+        $scope.getCanvasStyle = function (){
+           return {width: $scope.canvasWidth + 'px'};
+        }});
 
     $scope.getBoard = function () {
         boardManager.get('52ff5e6044ae2e25ae9712ae').then(function(board) {
             $scope.board = board;
+
+            var deckWidth = 250;
+            $scope.canvasWidth = board.decks.length * (deckWidth + 100);
         });
     };
 
