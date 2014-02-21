63e14785965fd1bb304c7a8c3e805469b51e54cf
Itay Maoz <itaym2@gmail.com>
2014-02-21 16:53:14 +0200

    update the page width after creating a new deck

--- public/javascripts/controllers/boardCtrl.js
+++ public/javascripts/controllers/boardCtrl.js
@@ -14,24 +14,28 @@ taskimApp.controller('boardCtrl',['$scope','boardManager',function($scope,boardM
 
     $scope.getBoard = function () {
         boardManager.get('52ff5e6044ae2e25ae9712ae').then(function(board) {
-            $scope.board = board;
-
-            var deckWidth = 250;
-            $scope.canvasWidth = board.decks.length * (deckWidth + 100);
+            $scope.setBoard(board);
         });
     };
 
+    $scope.setBoard = function(board) {
+        $scope.board = board;
+
+        var deckWidth = 250;
+        $scope.canvasWidth = board.decks.length * (deckWidth + 100);
+    }
+
     $scope.updateBoard = function() {
          boardManager.update($scope.board).then(function (board) {
-            $scope.board = board;
+            $scope.setBoard(board);
         });
     };
 
     $scope.createNewDeck = function(){
         var newDeck = {name: $scope.newDeckName, _id: ""};
         $scope.board.decks.push(newDeck);
-
         $scope.updateBoard();
+        $scope.newDeckName = "";
     };
 
     $scope.createNewCard = function(deckIndex, cardTitle) {
