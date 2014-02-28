b24211dd096dd925249fb032bc08a2baeccc3b55
Itay Maoz <itaym2@gmail.com>
2014-02-28 11:30:59 +0200

    made the card and deck menus open on top of other elements

--- public/app.js
+++ public/app.js
@@ -52,4 +52,17 @@ taskimApp.directive('resize', function ($window) {
             scope.$apply();
         });
     }
-})
+});
+
+taskimApp.directive('uplevel', function ($window) {
+    return function (scope, element) {
+        $(element).on('shown.bs.dropdown', function(event) {
+            var glyphicon = $(this).find('.glyphicon ');
+            var dropDown = $(this).find('.dropdown-menu');
+            var pos = dropDown.offset();
+            dropDown.css({ position: "fixed",
+                marginLeft: 0, marginTop: 0,
+                top: pos.top, left: pos.left });
+        });
+    }
+});
