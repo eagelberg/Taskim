eab7e4777741c4a8bfa56e2e9d08ebbf19d0bb04
Itay Maoz <itaym2@gmail.com>
2014-03-08 11:53:53 +0200

    small change in deck directive

--- public/javascripts/Directives/deck.js
+++ public/javascripts/Directives/deck.js
@@ -1,6 +1,7 @@
 taskimApp.directive('deck', function ($window) {
     return {
         restrict: 'E',
+        templateUrl: '/assets/partials/deck.html',
         link: function (scope, element) {
             var w = angular.element($window);
             scope.getWindowDimensions = function () {
@@ -10,14 +11,10 @@ taskimApp.directive('deck', function ($window) {
             scope.setHeight = function (newValue, oldValue) {
 
                 scope.maxHeight = (newValue - 70);
-
                 var e = element.get(0);
 
-                if(e.offsetHeight < e.scrollHeight) {
-                    if(e.offsetHeight < newValue) {
-                        scope.elementHeight = '100%';
-                    }
-
+                if((e.offsetHeight < e.scrollHeight) && (e.offsetHeight < newValue)) {
+                    scope.elementHeight = '100%';
                 }
             }
 
@@ -26,7 +23,6 @@ taskimApp.directive('deck', function ($window) {
             w.bind('resize', function () {
                 scope.$apply();
             });
-        },
-        templateUrl: '/assets/partials/deck.html'
+        }
     };
 });
