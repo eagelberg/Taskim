f0c89e84998146cf7e2b24807af8b714240b48b7
Itay Maoz <itaym2@gmail.com>
2014-04-18 18:07:00 +0300

    add comment list to card

--- app/Domain/Models/Card.java
+++ app/Domain/Models/Card.java
@@ -4,12 +4,22 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.mongojack.ObjectId;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card {
 
     @JsonProperty("_id")
     private String id;
     private String title;
     private String description;
+    private List<Comment> comments = new ArrayList<>();
+
+   public Card(){
+       Comment comment = new Comment();
+       comment.setContent("test");
+       comments.add(comment);
+   }
 
     //<editor-fold desc="Getters and Setters">
     public String getId() { return id; }
@@ -24,5 +34,11 @@ public class Card {
 
     public String getDescription() { return description; }
     public void setDescription(String description) {this.description = description;}
+
+    public List<Comment> getComments() {
+        List<Comment> commentsCopy = new ArrayList<>();
+        commentsCopy.addAll(comments);
+        return comments;
+    }
     //</editor-fold>
 }
